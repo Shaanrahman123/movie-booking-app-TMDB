f9b9a658a4c62da7b56024ebfdfdb58f7ebd72b4
shaanrahman123 <coderraufur@gmail.com>
2023-02-22 15:39:29 +0530

    update now

--- script.js
+++ script.js
@@ -7,28 +7,26 @@ modeBtn.onchange = (e) => {
   if (modeBtn.checked === true) {
     document.documentElement.classList.remove("light")
     document.documentElement.classList.add("dark")
-    window.localStorage.setItem('mode', 'dark');
-    console.log("Dark")
+    window.localStorage.setItem('checkbox', 'dark');
   } else {
     document.documentElement.classList.remove("dark")
     document.documentElement.classList.add("light")
-    window.localStorage.setItem('mode', 'light');
-    console.log("light")
+    window.localStorage.setItem('checkbox', 'light');
   }
 }
 
-// const mode = window.localStorage.getItem('checkbox');
-// if (mode == 'dark') {
-//   modeBtn.checked = true;
-//   document.documentElement.classList.remove("light")
-//   document.documentElement.classList.add("dark")
-// }
+const mode = window.localStorage.getItem('checkbox');
+if (mode == 'dark') {
+  modeBtn.checked = true;
+  document.documentElement.classList.remove("light")
+  document.documentElement.classList.add("dark")
+}
 
-// if (mode == 'light') {
-//   modeBtn.checked = false;
-//   document.documentElement.classList.remove("dark")
-//   document.documentElement.classList.add("light")
-// }
+if (mode == 'light') {
+  modeBtn.checked = false;
+  document.documentElement.classList.remove("dark")
+  document.documentElement.classList.add("light")
+}
 
 
 
